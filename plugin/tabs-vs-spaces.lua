38d98899cb1f76237165fe4622f119437cf071f6
Gustav Behm <me@rootmos.io>
2023-03-07 09:44:22 +0100

    Retab

--- plugin/tabs-vs-spaces.lua
+++ plugin/tabs-vs-spaces.lua
@@ -1,19 +1,19 @@
 vim.api.nvim_create_autocmd("BufEnter", {
-	pattern = "*",
-	callback = function(args)
-		local ft = vim.opt.filetype:get()
-		local C = require("tabs-vs-spaces.config")
-		local c = C[ft]
+    pattern = "*",
+    callback = function(args)
+        local ft = vim.opt.filetype:get()
+        local C = require("tabs-vs-spaces.config")
+        local c = C[ft]
 
-		if c == false then
-			return
-		elseif c == nil then
-			c = C.default
-			if c == false then
-				return
-			end
-		end
+        if c == false then
+            return
+        elseif c == nil then
+            c = C.default
+            if c == false then
+                return
+            end
+        end
 
-		require("tabs-vs-spaces")(args.buf, c)
-	end,
+        require("tabs-vs-spaces")(args.buf, c)
+    end,
 })
