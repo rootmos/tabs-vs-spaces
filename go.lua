f67d160fdc2438cf22cedeaa181ce8fa47117eaa
Gustav Behm <me@rootmos.io>
2023-03-08 09:46:38 +0100

    Remove vimscript code

--- go.lua
+++
@@ -1,7 +0,0 @@
-local S = require("ser")
-
-local P = require("tabs-vs-spaces")
-
-print(P.inspect(io.open("tests/examples/real-world.c")):decide())
-print(P.inspect(io.open("tests/examples/tiny-spaces.c")):decide())
-print(P.inspect(io.open("tests/examples/tiny-tabs.c")):decide())
