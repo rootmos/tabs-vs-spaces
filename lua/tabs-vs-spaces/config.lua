c289aad6d4b945433833cfd70c8bb74bed4a3054
Gustav Behm <me@rootmos.io>
2023-03-08 11:34:56 +0100

    Enable nicer configuration syntax

--- lua/tabs-vs-spaces/config.lua
+++ lua/tabs-vs-spaces/config.lua
@@ -1,3 +1,11 @@
-return {
-	default = false,
+local config = {
+    default = false,
 }
+
+return setmetatable(config, {
+    __call = function(c, o)
+        for k, v in pairs(o) do
+            c[k] = v
+        end
+    end,
+})
